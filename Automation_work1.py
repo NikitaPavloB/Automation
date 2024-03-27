066d362db23e9af7c04593c2110ce91bf2bb886e
Nikita_Pavlov <126702146+NikitaPavloB@users.noreply.github.com>
2024-03-27 21:24:06 +0300

    Update Automation_work1.py

--- Automation_work1.py
+++ Automation_work1.py
@@ -1,10 +1,29 @@
 import subprocess
+import string
 
-res = subprocess.run("cat /etc/os-release", shell=True, stdout=subprocess.PIPE, encoding='utf-8')
-out = res.stdout
-if not res.returncode:
-    lst = out.split("\n")
-    if 'VERSION="22.04.1 LTS (Jammy Jellyfish)"' in lst and 'VERSION_CODENAME=jammy' in lst:
-        print("SUCCESS")
+def check_output_mode(text, mode='line'):
+    if mode == 'line':
+        res = subprocess.run(text, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+        output = res.stdout.split("\n")
+    elif mode == 'word':
+        res = subprocess.run(text, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+        output = res.stdout.split()
+        output = [''.join(filter(lambda x: x not in string.punctuation, word)) for word in output]
+    else:
+        raise ValueError("Unsupported mode. Please choose either 'line' or 'word'.")
+    
+    if not res.returncode:
+        return output
+    else:
+        return []
+
+output_lines = check_output_mode("cat /etc/os-release", mode='line')
+output_words = check_output_mode("cat /etc/os-release", mode='word')
+
+if 'VERSION="22.04.1 LTS (Jammy Jellyfish)"' in output_lines and 'VERSION_CODENAME=jammy' in output_lines:
+    print("SUCCESS")
+
+if 'VERSION' in output_words:
+    print("The word 'VERSION' is present in the output.")
 else:
-    print("FAIL")
+    print("The word 'VERSION' is not present in the output.")
