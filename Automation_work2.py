1921545182bba657f8bdee478d995f9336d8d144
Nikita_Pavlov <126702146+NikitaPavloB@users.noreply.github.com>
2024-03-28 01:22:05 +0300

    Update Automation_work2.py

--- Automation_work2.py
+++ Automation_work2.py
@@ -1,45 +1,31 @@
 import subprocess
-import string
+FOLDER_TST = "/home/edgar/tst"
+FOLDER_OUT = "/home/edgar/out"
+FOLDER_1 = "/home/edgar/folder1"
 
-def check_output_mode(text, mode='line'):
-    if mode == 'line':
-        res = subprocess.run(text, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
-        output = res.stdout.split("\n")
-    elif mode == 'word':
-        res = subprocess.run(text, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
-        output = res.stdout.split()
-        output = [''.join(filter(lambda x: x not in string.punctuation, word)) for word in output]
+def checkout(cmd, text):
+    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+    if text in result.stdout and result.returncode == 0:
+        return True
     else:
-        raise ValueError("Unsupported mode. Please choose either 'line' or 'word'.")
-    
-    if not res.returncode:
-        return output
-    else:
-        return []
+        return False
 
-def test_ls_command():
-    # Проверка вывода списка файлов
-    output = check_output_mode("ls", mode='line')
-    assert isinstance(output, list)
-    assert len(output) > 0
-
-def test_extract_command():
-    # Проверка разархивирования с путями
-    res = subprocess.run("7z x test_archive.7z -ooutput_folder", shell=True)
-    assert res.returncode == 0
-    assert "Everything is OK" in res.stdout
-
-if __name__ == "__main__":
-    output_lines = check_output_mode("cat /etc/os-release", mode='line')
-    output_words = check_output_mode("cat /etc/os-release", mode='word')
-
-    print("Words from the output:")
-    print(output_words)  # Вывод списка слов из вывода
-
-    if 'VERSION="22.04.1 LTS (Jammy Jellyfish)"' in output_lines and 'VERSION_CODENAME=jammy' in output_lines:
-        print("SUCCESS")
-
-    if 'VERSION' in output_words:
-        print("The word 'VERSION' is present in the output.")
-    else:
-        print("The word 'VERSION' is not present in the output.")
+def test_step1():
+    # test1
+    res1 = checkout(f"cd {FOLDER_TST}; 7z a {FOLDER_OUT}/arx2.7z", "Everything is Ok")
+    res2 = checkout(f"ls {FOLDER_OUT}", "arx2.7z")
+    assert res1 and res2, "test1 FAIL"
+def test_step2():
+    # test2
+    res1 = checkout(f"cd {FOLDER_TST}; 7z e {FOLDER_OUT}/arx2.7z -o{FOLDER_1} -y", "Everything is Ok")
+    res2 = checkout(f"ls {FOLDER_1}", "text1.txt")
+    assert res1 and res2, "test2 FAIL"
+def test_step3():
+    # test3
+    assert checkout(f"cd {FOLDER_OUT}; 7z t arx2.7z", "Everything is Ok"), "test3 FAIL"
+def test_step4():
+    # test4
+    assert checkout(f"cd {FOLDER_TST}; 7z d .{FOLDER_OUT}/arx2.7z", "Everything is Ok"), "test4 FAIL"
+def test_step5():
+    # test5
+    assert checkout(f"cd {FOLDER_TST}; 7z u {FOLDER_OUT}/arx2.7z", "Everything is Ok"), "test5 FAIL"
