99875d4d4977e82dab24094105ddcb887734cff7
Nikita_Pavlov <126702146+NikitaPavloB@users.noreply.github.com>
2024-03-28 17:12:30 +0300

    Update Automation_work3.py

--- Automation_work3.py
+++ Automation_work3.py
@@ -1,67 +1,62 @@
-import subprocess
 import yaml
-from checkers import checkout
+from checkers import checkout, getout
+import os
+import secrets
 
-with open("config.yaml") as f:
+with open('config.yaml') as f:
     data = yaml.safe_load(f)
 
-FOLDER_TST = data["FOLDER_TST"]
-FOLDER_OUT = data["FOLDER_OUT"]
-FOLDER_1 = data["FOLDER_1"]
+class TestPositive:
+    def test_step1(self, make_folders, clear_folders, make_files, print_time):
+        cmd = "7z a {}/arx -t{}".format(data["folder_out"], data["type"])
+        res1 = checkout(cmd, "Everything is Ok")
+        cmd = "ls {}".format(data["folder_out"])
+        res2 = checkout(cmd, "arx.{}".format(data["type"]))
+        assert res1 and res2, "test1 FAIL"
 
-# def test_step1(clear_folders, make_folders, make_files):
-#     # test1
-#     res = []
-#     res.append(checkout(f"cd {FOLDER_TST}; 7z a {FOLDER_OUT}/arx2.7z", "Everything is Ok"))
-#     res.append(checkout(f"ls {FOLDER_OUT}", "arx2.7z"))
-#     assert all(res), "test1 FAIL"
+    def test_step2(self, clear_folders, make_files):
+        res = []
+        cmd = "7z a {}/arx -t{}".format(data["folder_out"], data["type"])
+        res.append(checkout(cmd, "Everything is Ok"))
+        cmd = "7z e arx.{} -o{} -y".format(data["type"], data["folder_ext"])
+        res.append(checkout(cmd, "Everything is Ok"))
+        for item in make_files:
+            cmd = "ls {}".format(data["folder_ext"])
+            res.append(checkout(cmd, item))
+        assert all(res)
 
-def test_step1(clear_folders, make_folders, make_files):
-    # test1
-    res1 = checkout(f"cd {FOLDER_TST}; 7z a {FOLDER_OUT}/arx2.7z", "Everything is Ok")
-    res2 = checkout(f"ls {FOLDER_OUT}", "arx2.7z")
-    assert res1 and res2, "test1 FAIL"
+    def test_step3(self):
+        cmd = "7z t arx.{}".format(data["type"])
+        assert checkout(cmd, "Everything is Ok"), "test3 FAIL"
 
+    def test_step4(self):
+        cmd = "7z u arx2.{}".format(data["type"])
+        assert checkout(cmd, "Everything is Ok"), "test4 FAIL"
 
-def test_step2():
-    # test2
-    res1 = checkout(f"cd {FOLDER_TST}; 7z e {FOLDER_OUT}/arx2.7z -o{FOLDER_1} -y", "Everything is Ok")
-    res2 = checkout(f"ls {FOLDER_1}", "text.txt")
-    assert res1 and res2, "test2 FAIL"
+    def test_step5(self, clear_folders, make_files):
+        res = []
+        cmd = "7z a {}/arx -t{}".format(data["folder_out"], data["type"])
+        res.append(checkout(cmd, "Everything is Ok"))
+        for i in make_files:
+            cmd = "7z l arx.{}".format(data["type"])
+            res.append(checkout(cmd, i))
+        assert all(res), "test5 FAIL"
 
+    def test_step6(self, clear_folders, make_files, make_subfolder):
+        res = []
+        cmd = "7z a {}/arx -t{}".format(data["folder_out"], data["type"])
+        res.append(checkout(cmd, "Everything is Ok"))
+        cmd = "7z x arx.{} -o{} -y".format(data["type"], data["folder_ext2"])
+        res.append(checkout(cmd, "Everything is Ok"))
+        for i in make_files:
+            cmd = "ls {}".format(data["folder_ext2"])
+            res.append(checkout(cmd, i))
+        cmd = "ls {}".format(data["folder_ext2"])
+        res.append(checkout(cmd, make_subfolder[0]))
+        cmd = "ls {}/{}".format(data["folder_ext2"], make_subfolder[0])
+        res.append(checkout(cmd, make_subfolder[1]))
+        assert all(res), "test6 FAIL"
 
-def test_step3():
-    # test3
-    assert checkout(f"cd {FOLDER_OUT}; 7z t arx2.7z", "Everything is Ok"), "test3 FAIL"
-
-
-def test_step4():
-    # test4
-    assert checkout(f"7z d {FOLDER_OUT}/arx2.7z", "Everything is Ok"), "test4 FAIL"
-
-
-def test_step5():
-    # test5
-    assert checkout(f"cd {FOLDER_TST}; 7z u {FOLDER_OUT}/arx2.7z", "Everything is Ok"), "test5 FAIL"
-
-
-def test_step6():
-    # test6 - проверка команды для вывода списка файлов (l)
-    res = checkout(f"cd {FOLDER_TST}; 7z l {FOLDER_OUT}/arx2.7z", "text.txt")
-    assert res, "test6 FAIL"
-
-
-def test_step7(clear_folders, make_folders, make_files):
-    # test7 - проверка команды для разархивирования с сохранением путей (x)
-    res = []
-    res.append(checkout(f"cd {FOLDER_TST}; 7z a {FOLDER_OUT}/arx2.7z", "Everything is Ok"))
-    res.append(checkout(f"cd {FOLDER_TST}; 7z x {FOLDER_OUT}/arx2.7z -o{FOLDER_1} -y", "Everything is Ok"))
-    for item in make_files:
-        res.append(checkout(f"ls {FOLDER_1}", item))
-    assert all(res), "test7 FAIL"
-
-
-def test_step8():
-    # test8 - проверка команды для расчета хеша (h)
-    res = checkout(f"cd {FOLDER_TST}; 7z h {FOLDER_OUT}/arx2.7z", "CRC32")
-    assert res, "test8 FAIL"
+    def test_step7(self):
+        cmd = "7z d arx.{}".format(data["type"])
+       
