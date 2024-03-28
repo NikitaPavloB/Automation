624a461bbbead0c96dbe077970514746fbd164fd
Nikita_Pavlov <126702146+NikitaPavloB@users.noreply.github.com>
2024-03-28 12:06:29 +0300

    Create Automation_work3.py

---
+++ Automation_work3.py
@@ -0,0 +1,64 @@
+import subprocess
+import yaml
+
+with open("config.yaml") as f:
+    data = yaml.safe_load(f)
+FOLDER_TST = data["FOLDER_TST"]
+FOLDER_OUT = data["FOLDER_OUT"]
+FOLDER_1 = data["FOLDER_1"]
+
+
+def checkout(cmd, text):
+    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+    if text in result.stdout and result.returncode == 0:
+        return True
+    else:
+        return False
+
+
+def test_step1():
+    # test1
+    res1 = checkout(f"cd {FOLDER_TST}; 7z a {FOLDER_OUT}/arx2.7z", "Everything is Ok")
+    res2 = checkout(f"ls {FOLDER_OUT}", "arx2.7z")
+    assert res1 and res2, "test1 FAIL"
+
+
+def test_step2():
+    # test2
+    res1 = checkout(f"cd {FOLDER_TST}; 7z e {FOLDER_OUT}/arx2.7z -o{FOLDER_1} -y", "Everything is Ok")
+    res2 = checkout(f"ls {FOLDER_1}", "text.txt")
+    assert res1 and res2, "test2 FAIL"
+
+
+def test_step3():
+    # test3
+    assert checkout(f"cd {FOLDER_OUT}; 7z t arx2.7z", "Everything is Ok"), "test3 FAIL"
+
+
+def test_step4():
+    # test4
+    assert checkout(f"7z d {FOLDER_OUT}/arx2.7z", "Everything is Ok"), "test4 FAIL"
+
+
+def test_step5():
+    # test5
+    assert checkout(f"cd {FOLDER_TST}; 7z u {FOLDER_OUT}/arx2.7z", "Everything is Ok"), "test5 FAIL"
+
+
+def test_step6():
+    # test6 - проверка команды для вывода списка файлов (l)
+    res = checkout(f"cd {FOLDER_TST}; 7z l {FOLDER_OUT}/arx2.7z", "text.txt")
+    assert res, "test6 FAIL"
+
+
+def test_step7():
+    # test7 - проверка команды для разархивирования с сохранением путей (x)
+    res = checkout(f"cd {FOLDER_TST}; 7z x {FOLDER_OUT}/arx2.7z -o{FOLDER_1} -y", "Everything is Ok")
+    res2 = checkout(f"ls {FOLDER_1}", "text.txt")
+    assert res and res2, "test7 FAIL"
+
+
+def test_step8():
+    # test8 - проверка команды для расчета хеша (h)
+    res = checkout(f"cd {FOLDER_TST}; 7z h {FOLDER_OUT}/arx2.7z", "CRC32")
+    assert res, "test8 FAIL"
